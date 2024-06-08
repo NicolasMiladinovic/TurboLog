11cd1036f28d23dc461b7925d120c0eeda6aebfe
NicolasMiladinovic <nicolas.miladinovic@gmail.com>
2024-06-09 00:44:53 +0200

    [feat] Get highlighted text

---
+++ out/extension.js
@@ -0,0 +1,47 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const vscode = __importStar(require("vscode"));
+function activate(context) {
+    console.log('GGWP, your extension "turbolog" is now active!');
+    const disposable = vscode.commands.registerCommand('turbolog.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from TurboLog! gg');
+        // Get selected text
+        const editor = vscode.window.activeTextEditor;
+        const selection = editor?.selection;
+        if (selection && !selection.isEmpty) {
+            const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+            const highlighted = editor?.document.getText(selectionRange);
+            console.log('highlighted', highlighted);
+        }
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+// This method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
